cd1de2c7d6adc3dfa146236d3a493dc789bc3b88
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-06 16:36:14 -0300

    exceções e coleções com c#

---
+++ plataforma-net/excecoes-colecoes/Program.cs
@@ -0,0 +1,181 @@
+﻿using System.Globalization;
+using excecoes_colecoes.Models;
+
+// [1.2]: Realizando a leitura de um arquivo
+
+/* string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+foreach (string linha in linhas)
+{
+    Console.WriteLine(linha);
+} */
+
+// [1.3]: Disparando uma exceção
+// [1.4]: Tratando uma exceção
+
+/* try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+} */
+
+// [1.5]: Exceção genérica e específica
+
+/* try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/d/arquivo_Leitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do aquivo. Arquivo não encontrado. {ex.Message}");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do aquivo. Caminho da pasta não encontrado. {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+} */
+
+// [1.6]: Entendendo o bloco finaly
+
+/* try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/d/arquivo_Leitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do aquivo. Arquivo não encontrado. {ex.Message}");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do aquivo. Caminho da pasta não encontrado. {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+} 
+finally
+{
+    Console.WriteLine("Chegou até aqui!");
+} */
+
+// [1.7]: Usando o throw
+
+/* new ExemploExcecao().Metodo1(); */
+
+// [1.9]: Fila na prática
+
+/* Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+fila.Enqueue(10);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+} */
+
+// [1.11]: Pilha na prática
+
+/* Stack<int> pilha = new Stack<int>();
+
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+
+pilha.Push(20);
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);
+} */
+
+// [1.12]: Introdução Dictionary
+
+/* Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minhas Gerais");
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+estados.Add("BA2", "Bahia"); */
+
+// [1.13]: Removendo e alterando elementos
+
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minhas Gerais");
+
+Console.WriteLine(estados["MG"]);
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+Console.WriteLine("--------------------------------");
+
+estados.Remove("BA");
+estados["SP"] = "São Paulo - valor alterado!";
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+string chave = "BA2";
+Console.WriteLine("Verificando o elemento: ");
+
+if (estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: {chave}");
+}
+else
+{
+    Console.WriteLine($"Valor não existe. É seguro adicionar a chave: {chave}");
+}
