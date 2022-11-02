af03c61a5756d1b9c7b6faf373cf0e8972537c8a
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-11-02 12:06:10 -0300

    frontend aspnet mvc

---
+++ dominando-ecossistema-net/frontend-asp-net-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace frontend_asp_net_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
